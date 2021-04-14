ead90f9c5871a6edde2b96894400bc58601366f6
xeropise <whrbql1@gmail.com>
2021-04-15 00:06:07 +0900

    testing

---
+++ BigDataBatchSample/src/test/appendFile-test.ts
@@ -0,0 +1,12 @@
+import * as path from 'path'
+import { appendFile } from '../fileApi/appendFile'
+import { mkdir } from '../fileApi/mkdir'
+
+const appendTest = async(filename: string, data: any) => {
+    const result = await appendFile(filename, data)
+    console.log(`append ${result} to ${filename}`)
+}
+
+mkdir('./data')
+    .then(s => appendTest('.,/data/hello.txt', 'Hi, there!'))
+    .catch((e: Error) => console.log(e.message))
