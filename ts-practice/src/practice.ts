558c7ee3bd0aa5466fcb8d9af3d331f2a5146a07
xeropise <whrbql1@gmail.com>
2021-04-14 23:54:43 +0900

    test commit

---
+++ ts-practice/src/practice.ts
@@ -0,0 +1,14 @@
+
+let count = 0;
+count += 1;
+
+const message: string = 'hello world';
+
+const done: boolean = true; 
+
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ['hello', 'world'];
+
+//messages.push(1); // 숫자를 넣으려고하면 오류
+
+let mightBeUndefined: string | undefined = undefined
